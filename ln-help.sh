8c3c4a226b055b5e4e9706c642d975719ecddfea
殊途 <shutu736@gmail.com>
2022-07-15 23:37:39 +0800

    aaa

--- ln-help.sh
+++ ln-help.sh
@@ -6,11 +6,12 @@
 # Description:          ln扩容
 
 vol=$1
+username=$2
 
 mkfs.ext4 "/dev/disk/by-id/scsi-0Linode_Volume_${vol}"
 mkdir "/mnt/${vol}"
 mount "/dev/disk/by-id/scsi-0Linode_Volume_${vol}" "/mnt/${vol}"
 /dev/disk/by-id/scsi-0Linode_Volume_${vol} /mnt/${vol} ext4 defaults,noatime,nofail 0 2
-mkdir -p /mnt/vol/Download
-chown devdong /mnt/vol/Download/
-sed -i "s#Downloads\SavePath=/home/${USERNAME}/Downloads/#Downloads\SavePath=/mnt/vol/Download/#g" /home/${USERNAME}/.config/qBittorrent/qBittorrent.conf 
+mkdir -p /mnt/${vol}/Download
+chown ${username} /mnt/${vol}/Download/
+sed -i "s#Downloads\SavePath=/home/${username}/Downloads/#Downloads\SavePath=/mnt/vol/Download/#g" /home/${username}/.config/qBittorrent/qBittorrent.conf 
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 apt-get update && apt-get install bash wget -y && bash <(wget -qO- "https://raw.githubusercontent.com/shutu777/help/main/dd-help.sh")
 ```
 ```bash
-bash <(wget -qO- "https://raw.githubusercontent.com/shutu777/help/main/ln-help.sh")
+bash <(wget -qO- "https://raw.githubusercontent.com/shutu777/help/main/ln-help.sh") vol
 ```
 ```bash
 apt-get update && apt-get install bash wget -y && bash <(wget -qO- "https://raw.githubusercontent.com/shutu777/help/main/zx.sh")
